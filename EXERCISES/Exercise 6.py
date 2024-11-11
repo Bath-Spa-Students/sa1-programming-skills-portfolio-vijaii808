21158ac0c2d6866b3dc89d8c659dec94891afd75
vijaii808 <vijaipragadeesh8@gmail.com>
2024-11-11 22:23:06 +0400

    Delete EXERCISES directory

--- EXERCISES/Exercise 6.py
+++
@@ -1,9 +0,0 @@
-correct_password = "12345"
-while True:
-    user_input = input("Please enter the password: ")
-
-    if (user_input) == correct_password:
-        print("Access granted!")
-        break
-    else:
-        print("Incorrect password, please try again.")
