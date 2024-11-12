eb454ea127a23e58e585864f139c5ef44b54e3ce
vijaii808 <vijaipragadeesh8@gmail.com>
2024-11-12 10:21:58 +0400

    Create Exercise 3.py

---
+++ Exercise 3/Exercise 3.py
@@ -0,0 +1,13 @@
+Data ={"name": "vijai","hometown": "Dubai","age": 18}
+print(Data["name"])
+print(Data["hometown"])
+print(Data["age"])
+
+#advanced requirement
+name=input("please enter your name:")
+hometown=input("please enter your hometown:")
+age=input("please enter your age:")
+Data ={"name": name,"hometown":hometown,"age": age}
+print(Data["name"])
+print(Data["hometown"])
+print(Data["age"])
