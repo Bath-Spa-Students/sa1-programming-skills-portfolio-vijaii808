ebb7ed448709c4330d6cf53c4f3b03055dc96c84
vijaii808 <vijaipragadeesh8@gmail.com>
2024-11-12 10:22:02 +0400

    Create Exercise 4.py

---
+++ Exercise 4/Exercise 4.py
@@ -0,0 +1,17 @@
+#Write a program that asks the user “What is the capital of France?” and waits for their response.
+qs1=input("whats is the captial of france:")
+ans="france"
+if qs1 == ans:
+    print ("Correct!")
+
+else:
+    print("Wrong!")
+
+#Ignore Capitalization:
+qs1=input("whats is the captial of france:")
+ans="france"
+if qs1.lower() == ans.lower():
+    print ("Correct!")
+
+else:
+    print("Wrong!")
