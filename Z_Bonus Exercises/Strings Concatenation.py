0829256362eade23c6f8d8ea6948d67dbdaa4c14
vijaii808 <vijaipragadeesh8@gmail.com>
2024-11-20 18:40:47 +0400

    Create Strings Concatenation.py

---
+++ Z_Bonus Exercises/Strings Concatenation.py
@@ -0,0 +1,10 @@
+#String Concatention
+
+# Storing strings in different variables
+string1 = "Hello"
+string2 = "World"
+string3 = "!"
+
+# Concatenating and printing the output
+output = string1 + " " + string2 + string3
+print(output)
