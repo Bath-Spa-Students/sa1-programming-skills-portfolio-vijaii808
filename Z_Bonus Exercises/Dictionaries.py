d203123f9ac3552e9d7e2c9a8e0cfa654c607098
vijaii808 <vijaipragadeesh8@gmail.com>
2024-11-20 19:11:10 +0400

    Create Dictionaries.py

---
+++ Z_Bonus Exercises/Dictionaries.py
@@ -0,0 +1,11 @@
+Data = {
+    'first_name': 'Vijai',
+    'last_name': 'Pragadeesh',
+    'age': 18,
+    'city': 'Dubai'
+}
+# Printing each piece of information stored in the dictionary
+print("First name:", Data['first_name'])
+print("Last name:", Data['last_name'])
+print("Age:", Data['age'])
+print("City:", Data['city'])
