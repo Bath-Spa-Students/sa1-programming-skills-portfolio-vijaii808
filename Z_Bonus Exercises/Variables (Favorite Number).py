b78a4bfaa9d30fc9d64e70cbac473c0835a3506f
vijaii808 <vijaipragadeesh8@gmail.com>
2024-11-20 18:53:11 +0400

    Create Variables (Favorite Number).py

---
+++ Z_Bonus Exercises/Variables (Favorite Number).py
@@ -0,0 +1,8 @@
+# Assigning my favorite number
+fav_number = 14
+
+# Creating a message that reveals my favorite number
+message = "My fav number is " + str(fav_number)
+
+# Printing the message
+print(message)
